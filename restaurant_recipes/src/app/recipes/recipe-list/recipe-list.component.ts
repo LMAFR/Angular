cdf0db48eeb27e05d3cbe130184e5d16a0b8f0a1
LMAFR <alejandrofloridoreyes@gmail.com>
2022-11-21 18:00:06 +0100

    Test recipe instance created. HTML template for recipe-list updated.

--- restaurant_recipes/src/app/recipes/recipe-list/recipe-list.component.ts
+++ restaurant_recipes/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [new Recipe('A test recipe', 'This is simply a test', 'https://www.cookipedia.co.uk/wiki/images/e/ea/Gazpacho_ligero_con_mostaza_recipe.jpg')];
 
   constructor() { }
 
