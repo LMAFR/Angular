24740b99ee3250c84a717d9761800d45be8f909a
LMAFR <alejandrofloridoreyes@gmail.com>
2022-11-22 10:32:45 +0100

    Ingredient model created in shared folder, as it will affect both shopping list and recipe folders.

---
+++ restaurant_recipes/src/shared/ingredient.model.ts
@@ -0,0 +1,4 @@
+export class Ingredient{
+    // The line below is equivalent to define the model as we did for recipe (classical way vs. TS shortcut)
+    constructor(public name:string, public amount:number){}
+}
