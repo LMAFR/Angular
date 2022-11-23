c54751cdae5d2952899ea2eb700ddd15ee7f4f71
LMAFR <alejandrofloridoreyes@gmail.com>
2022-11-23 17:07:57 +0100

    Dynamic background added to paragraph using HostListener decorator.

--- directives-start/src/app/directives/better-highlight.directive.ts
+++ directives-start/src/app/directives/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Renderer2, OnInit} from '@angular/core';
+import { Directive, ElementRef, Renderer2, OnInit, HostListener} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
@@ -6,10 +6,20 @@ import { Directive, ElementRef, Renderer2, OnInit} from '@angular/core';
 export class BetterHighlightDirective implements OnInit {
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) { 
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
   }
 
   ngOnInit(): void {
     
   }
+
+  // The argument for the decorator is a specific argument, you cannot write anything there.
+  @HostListener('mouseenter') mouseover(eventData: Event){
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+  }
+  // The Listener below is also important, as we need it to return the paragraph to transparent background when the mouse leave the paragraph.
+  @HostListener('mouseleave') mouseleave(eventData: Event){
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color','transparent');
+  }
+
 }
