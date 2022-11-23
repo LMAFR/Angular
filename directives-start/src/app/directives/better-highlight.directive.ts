a060ae8d04168440f14801c08131a1a3982ac3c0
LMAFR <alejandrofloridoreyes@gmail.com>
2022-11-23 18:25:27 +0100

    Now the background color when the mouse is on or not on the second paragraph changes dynamically and the color can be chosen in the HTML template. Also, it has been tested what happens if the alias of an Input has the same name that the directive (in that case, the input value can be called by only adding square brackets to the directive name).

--- directives-start/src/app/directives/better-highlight.directive.ts
+++ directives-start/src/app/directives/better-highlight.directive.ts
@@ -1,25 +1,31 @@
-import { Directive, ElementRef, Renderer2, OnInit, HostListener} from '@angular/core';
+import { Directive, ElementRef, Renderer2, OnInit, HostListener, HostBinding, Input} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  @Input() defaultColor:string = 'transparent';
+  @Input('appBetterHighlight') highlightColor:string = 'blue';
+
+  @HostBinding('style.backgroundColor') backgroundColor:string;
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) { 
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
   }
 
   ngOnInit(): void {
-    
+    this.backgroundColor = this.defaultColor
   }
 
   // The argument for the decorator is a specific argument, you cannot write anything there.
   @HostListener('mouseenter') mouseover(eventData: Event){
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = this.highlightColor;
   }
   // The Listener below is also important, as we need it to return the paragraph to transparent background when the mouse leave the paragraph.
   @HostListener('mouseleave') mouseleave(eventData: Event){
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color','transparent');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color','transparent');
+    this.backgroundColor = this.defaultColor;
   }
 
 }
