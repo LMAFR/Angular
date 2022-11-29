c5b951a0aab5b5ad4b0229445c242f2e17ccb7bf
LMAFR <alejandrofloridoreyes@gmail.com>
2022-11-29 09:26:15 -0100

    Now Recipe model has also an ingredients variable which is reflected on the HTML template.

--- restaurant_recipes/src/app/recipes/recipe.service.ts
+++ restaurant_recipes/src/app/recipes/recipe.service.ts
@@ -1,11 +1,24 @@
 import { Recipe } from "./recipe.model";
 import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from "src/shared/ingredient.model";
 
 @Injectable()
 export class RecipeService {
     private recipes: Recipe[] = [
-        new Recipe('A test recipe', 'This is simply a test', 'https://www.cookipedia.co.uk/wiki/images/e/ea/Gazpacho_ligero_con_mostaza_recipe.jpg'),
-        new Recipe('Another test recipe', 'This is also a test', 'https://www.cookipedia.co.uk/wiki/images/e/ea/Gazpacho_ligero_con_mostaza_recipe.jpg')
+        new Recipe('A test recipe', 
+                    'This is simply a test', 
+                    'https://www.cookipedia.co.uk/wiki/images/e/ea/Gazpacho_ligero_con_mostaza_recipe.jpg',
+                    [ new Ingredient('Tomatoes', 5),
+                      new Ingredient('Jamon', 1),
+                      new Ingredient('Bread', 1)
+                    ]
+                    ),
+        new Recipe('Another test recipe', 
+                    'This is also a test',
+                    'https://upload.wikimedia.org/wikipedia/commons/9/93/Carl%27s_Jr_Big_Burger_%2841516298835%29.jpg',
+                    [new Ingredient('Bread', 1),
+                     new Ingredient('Meat', 1)]
+                     )
       ];
     recipeSelected = new EventEmitter<Recipe>();
     
