12bbd90771c0332e363de0c686813cc02bf4495b
LMAFR <alejandrofloridoreyes@gmail.com>
2022-11-30 10:58:04 +0000

    New feature added to change the path and the HTML template dynamically in the users and user case.

--- routing-start/src/app/users/user/user.component.ts
+++ routing-start/src/app/users/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -16,6 +16,14 @@ export class UserComponent implements OnInit {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
     }
+  // The new "Load Anna" link we created does not change the values in this.user, as the component is not destroyed and re-created,
+  // so we have to add a asynchronous listener to make those changes just in case someone click on Load Anna in a certain moment:
+  this.route.params.subscribe(
+    (params:Params) => {
+      this.user.id = params['id'];
+      this.user.name = params['name'];
+    }
+  )
   }
 
 }
