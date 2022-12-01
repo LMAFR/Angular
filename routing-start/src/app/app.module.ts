91b23e3c7171215517331248f20fd7c75476e2ad
LMAFR <alejandrofloridoreyes@gmail.com>
2022-12-01 14:18:11 +0000

    Routes have been outsourced creating a new TS file and configuring it as an Angular module.

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -13,21 +13,7 @@ import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
-const appRoutes: Routes = [
-  {path: '', component:HomeComponent},
-  {path:'users', component:UsersComponent, children:[
-    {path:':id/:name', component:UserComponent}
-  ]},
-  {path:'servers', component:ServersComponent, children:[
-    {path:':id', component:ServerComponent},
-    {path:':id/edit', component:EditServerComponent}
-  ]},
-  {path:'not-found', component:PageNotFoundComponent},
-// The line below shall be placed as the last one in evey project, as in other case all the routes created below it will be ignored
-  {path:'**', redirectTo:'/not-found'}
-  
-]
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -43,7 +29,7 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
