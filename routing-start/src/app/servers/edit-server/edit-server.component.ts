8ac9f7ca1d2321f57d954b49e1cc97a3a69c738a
LMAFR <alejandrofloridoreyes@gmail.com>
2022-12-02 11:44:35 +0000

    Now the page-not-found component (called now error-page-component) takes the error message directly from the routing file.

--- routing-start/src/app/servers/edit-server/edit-server.component.ts
+++ routing-start/src/app/servers/edit-server/edit-server.component.ts
@@ -24,6 +24,11 @@ export class EditServerComponent implements OnInit, CanComponentDeactivate {
     console.log(this.route.snapshot.fragment);
     const id = +this.route.snapshot.params['id'];
     this.server = this.serversService.getServer(id);
+    this.route.params.subscribe(
+      (params:Params) => {
+        this.server = this.serversService.getServer(+params['id']);
+      }
+    )
     this.route.queryParams.subscribe(
       (queryParams:Params) => {
         this.allowEdit = queryParams['allowEdit'] === '1' ? true : false
