929dd22112b74d8612304b06c486df087f6e1bcc
LMAFR <alejandrofloridoreyes@gmail.com>
2022-12-02 14:56:34 +0000

    The resolve Guard has been tested. It is used to get the servers ID from the routes we built for each individual server.

--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ActivatedRoute, Data, Params, Router } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -16,14 +16,24 @@ export class ServerComponent implements OnInit {
               private router:Router) { }
 
   ngOnInit() {
+
+    this.route.data
+      .subscribe(
+        (data:Data) => {
+          // In the line below, the argument of data has to be the same we wrote as key in the app-routing file when we call the argument "resolve".
+          this.server = data['server'];
+        }
+      );
+
+    //The lines below were commented because the server-resolver service does the same but better.
     // The plus symbol before "this" in the line below and "params" some lines below too, is there to convert the result of the call (a string) in a number, as id is defined as a number.
-    const id = +this.route.snapshot.params['id'];
-    this.server = this.serversService.getServer(id);
-    this.route.params.subscribe(
-      (params:Params) => {
-        this.server = this.serversService.getServer(+params['id']);
-      }
-    )
+    // const id = +this.route.snapshot.params['id'];
+    // this.server = this.serversService.getServer(id);
+    // this.route.params.subscribe(
+    //   (params:Params) => {
+    //     this.server = this.serversService.getServer(+params['id']);
+    //   }
+    // )
   }
 
   onEdit(){
