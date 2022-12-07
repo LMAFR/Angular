a684ce19706f07d0d588e02f8254055641e647c2
LMAFR <alejandrofloridoreyes@gmail.com>
2022-12-07 13:30:37 +0000

    The previous button feature is now implemented using a Subject instead of an EventEmitter.

--- obs-01-start/src/app/user.service.ts
+++ obs-01-start/src/app/user.service.ts
@@ -1,5 +1,8 @@
-import { EventEmitter } from "@angular/core";
+// import { EventEmitter } from "@angular/core";
+import { Subject } from "rxjs";
 
 export class UserService {
-    activatedEmitter = new EventEmitter<boolean>();
+    // Subjects are more efficient than EventEmitter for crosscomponent uses (as here, where we are using a service in several components)
+    // or when we have to subscribe. 
+    activatedEmitter = new Subject<boolean>();
 }
