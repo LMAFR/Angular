6e917fd84d682714e17316c24e36c5704915ce77
LMAFR <alejandrofloridoreyes@gmail.com>
2022-12-07 10:19:50 +0000

    First observable created using interval() and count. ngOnDestroy was used in order to destroy the observable if the observed component changed.

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  firstObsSubscription: Subscription;
+
   constructor() { }
 
   ngOnInit() {
+    this.firstObsSubscription = interval(1000).subscribe(count => {
+      console.log(count)
+    });
+  }
+
+  ngOnDestroy() {
+    this.firstObsSubscription.unsubscribe();
   }
 
 }
