52cbabb8534f70f4fe6ee18df74c363e3455f8d2
LMAFR <alejandrofloridoreyes@gmail.com>
2022-12-07 11:54:08 +0000

    Map and Filter operators have been tested.

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { interval, Observable, Subscription } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -39,7 +40,11 @@ export class HomeComponent implements OnInit {
       }, 1000);
     })
 
-    this.firstObsSubscription = customIntervalObservable.subscribe( (data) => {
+    this.firstObsSubscription = customIntervalObservable.pipe(filter((data:number) => {
+      return data > 0;
+    }), map( (data:number) => {
+      return 'Round: ' + (data + 1);
+    })).subscribe( (data) => {
       console.log(data)
     }, error => {
       console.log(error);
