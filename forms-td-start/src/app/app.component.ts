c1f732c792505c215d6ae98f4f644236f3a9a7ad
LMAFR <alejandrofloridoreyes@gmail.com>
2022-12-12 10:34:43 +0000

    One field of the form can now be modified using the "Suggest an Username" button.

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -14,6 +14,24 @@ export class AppComponent {
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+
+    // Three ways to modify a part of the form. 1) Using two-way binding.
+    // 2) Using setValue() to set values for the whole form:
+    // this.signupForm.setValue({
+    //   userData:{
+    //     username:suggestedName,
+    //     email:'heredia@hotmail.com'
+    //   },
+    //   secret:'pet',
+    //   questionAnswer:'',
+    //   gender:'male'
+    // });
+    // 3) Using form.patchValue() to modify only N parts of the form:
+    this.signupForm.form.patchValue({
+      userData:{
+        username:suggestedName
+      }
+    })
   }
 
   // onSubmit(form: HTMLFormElement){
