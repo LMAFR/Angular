9b4834c881d91b0ce2b948eee9ad81049cb353be
LMAFR <alejandrofloridoreyes@gmail.com>
2022-12-12 11:49:45 +0000

    First basic form created.

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   genders = ['male', 'female'];
   signUpForm:FormGroup;
+
+  ngOnInit() {
+    this.signUpForm = new FormGroup({
+      'username': new FormControl(null),
+      'email': new FormControl(null),
+      // The first value in FormControl is the default value os that field in the form
+      'gender': new FormControl('male')
+    });
+  }
 }
