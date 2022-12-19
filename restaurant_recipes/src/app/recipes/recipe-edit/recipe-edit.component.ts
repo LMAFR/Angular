c9e60fc9daa798e7cd34eb0b7e1557d0e1391d50
LMAFR <alejandrofloridoreyes@gmail.com>
2022-12-19 17:15:03 +0000

    Now the delete and cancel buttons are working.

--- restaurant_recipes/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ restaurant_recipes/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -14,7 +14,7 @@ export class RecipeEditComponent implements OnInit {
   editMode: boolean = false;
   recipeForm: FormGroup;
 
-  constructor(private route:ActivatedRoute, private recipeService:RecipeService) { }
+  constructor(private route:ActivatedRoute, private recipeService:RecipeService, private router:Router) { }
 
   ngOnInit(): void {
     this.route.params
@@ -69,7 +69,7 @@ export class RecipeEditComponent implements OnInit {
     } else {
       this.recipeService.addRecipe(this.recipeForm.value);
     }
-
+    this.onCancel();
   }
 
   onAddIngredient(){
@@ -79,6 +79,10 @@ export class RecipeEditComponent implements OnInit {
         'amount': new FormControl('',[Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)])
       })
     );
+  }
+
+  onCancel(){
+    this.router.navigate(['../'], {relativeTo:this.route});
   }
 
 }
