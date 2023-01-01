dce379d29584e3606197f4a684a5772270f6b8f0
Alejandro Florido KPMG <alejandrofloridoreyes@gmail.com>
2023-01-01 13:14:50 +0100

    Pipe and map have been used to change the 'return' of the GET request. Now it returns an Array instead of an object.

--- http-start/src/app/app.component.ts
+++ http-start/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -33,7 +34,20 @@ export class AppComponent implements OnInit {
   }
 
   fetchPosts() {
-    this.http.get('https://ng-http-requests-163b0-default-rtdb.firebaseio.com/posts.json').subscribe(
+    this.http
+    .get('https://ng-http-requests-163b0-default-rtdb.firebaseio.com/posts.json')
+    .pipe(
+      map((responseData) => {
+        const postsArray = [];
+        for (const key in responseData){
+          if (responseData.hasOwnProperty(key)) {
+            postsArray.push({...responseData[key], id:key});
+          }
+        }
+        return postsArray;
+      })
+    )
+    .subscribe(
       (posts)=>{
         console.log(posts);
       }
