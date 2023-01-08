c4989b79fc4839822208d9b0c5eed06893aa7031
Alejandro Florido KPMG <alejandrofloridoreyes@gmail.com>
2023-01-08 17:07:45 +0100

    Testing how to add headers to http requests.

--- http-start/src/app/posts.service.ts
+++ http-start/src/app/posts.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Post } from "./post.model";
 import { catchError, map } from "rxjs/operators";
@@ -30,7 +30,12 @@ export class PostService {
     fetchPosts(){
         return this.http
         .get<{[key:string]: Post}>
-        ('https://ng-http-requests-163b0-default-rtdb.firebaseio.com/posts.json')
+        (
+          'https://ng-http-requests-163b0-default-rtdb.firebaseio.com/posts.json',
+          {
+            headers: new HttpHeaders({'Custom-header':'Hello'})
+          }
+        )
         .pipe(
           map((responseData) => {
             const postsArray: Post[] = [];
