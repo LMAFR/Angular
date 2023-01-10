5f62d1c032c30394dd604240eb3afd895ca3009e
Alejandro Florido KPMG <alejandrofloridoreyes@gmail.com>
2023-01-10 16:06:18 +0100

    Tiny adjustment to apply the changes introduced in recipes with the edit mode (last changes made them dissapear).

--- restaurant_recipes/src/app/recipes/recipes-resolver.service.ts
+++ restaurant_recipes/src/app/recipes/recipes-resolver.service.ts
@@ -2,13 +2,21 @@ import { Injectable } from "@angular/core";
 import { Recipe } from "./recipe.model";
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from "@angular/router";
 import { DataStorageService } from "src/shared/data-storage.service";
+import { RecipeService } from "./recipe.service";
 
 
 @Injectable({providedIn:'root'})
 export class RecipesResolverService implements Resolve<Recipe[]> {
-    constructor(private dataStorageService:DataStorageService) {}
+    constructor(private dataStorageService:DataStorageService, private recipeService:RecipeService) {}
 
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot){
-        return this.dataStorageService.fetchRecipes();
+        const recipes = this.recipeService.getRecipes();
+        
+        if (recipes.length === 0){
+            return this.dataStorageService.fetchRecipes();
+        }
+        else {
+            return recipes;
+        }
     }
 } 
