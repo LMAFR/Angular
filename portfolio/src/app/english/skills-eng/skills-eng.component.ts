c89406825fd6fb767956043c5969fbac9ec76007
LMAFR <alejandrofloridoreyes@gmail.com>
2023-01-20 22:31:52 +0000

    Now the bars and images in the 'about' section redirect to my GitHub repositories (in case it exists).

--- portfolio/src/app/english/skills-eng/skills-eng.component.ts
+++ portfolio/src/app/english/skills-eng/skills-eng.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class SkillsEngComponent {
 
+  onAngularRepository(){
+    location.href = "https://github.com/LMAFR/Angular";
+  };
+
+  onDjangoRepository(){
+    location.href = "https://github.com/LMAFR/Django";
+  };
+
+  onFlaskRepository(){
+    location.href = "https://github.com/LMAFR/Flask";
+  };
+
+  onReactRepository(){
+    location.href = "https://github.com/LMAFR/React";
+  };
+
+  onRustRepository(){
+    location.href = "https://github.com/LMAFR/Rust";
+  };
 }
