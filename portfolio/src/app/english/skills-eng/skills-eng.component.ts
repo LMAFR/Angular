b5eb9244a0dd33c53c34592142870e85ce528a2b
LMAFR <alejandrofloridoreyes@gmail.com>
2023-02-13 22:11:54 +0000

    First steps to make the skill bar animation work only if they are visible in the website.

--- portfolio/src/app/english/skills-eng/skills-eng.component.ts
+++ portfolio/src/app/english/skills-eng/skills-eng.component.ts
@@ -1,11 +1,35 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-skills-eng',
   templateUrl: './skills-eng.component.html',
-  styleUrls: ['./skills-eng.component.css']
+  styleUrls: ['./skills-eng.component.css'],
+  animations: [trigger ('skillBarGrowth', [
+    transition(':enter', [
+      // Initial state of element
+      style({width:0}),
+      // Final state of element after it enters in the view (DOM)
+      animate('1s', style({width:100}))
+    ]),
+    transition(':leave', [
+      // Final state of element after it goes out of the view (DOM)
+      animate('2.5s', style({width:0}))
+    ])
+  ])]
 })
 export class SkillsEngComponent {
+
+  isShown:boolean = false;
+
+  // I still have to make this function trigger when it surpasses the elements with the class skill-per
+  inViewPort(){
+    if (document.querySelectorAll('[skill-per]')){
+      this.isShown = true;
+    } else {
+      this.isShown = false;
+    }
+  }
 
   onAngularRepository(){
     location.href = "https://github.com/LMAFR/Angular";
