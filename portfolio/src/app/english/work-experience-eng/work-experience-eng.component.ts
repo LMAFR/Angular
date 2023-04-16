e74550c2416de0640498196236451594fa152dd7
LMAFR <alejandrofloridoreyes@gmail.com>
2023-04-16 15:55:50 +0100

    Now the work experience is provided in the Typescript file. The calculation of the time spent in each company has also been modified to depend on the variable that stores all the work experience information.

--- portfolio/src/app/english/work-experience-eng/work-experience-eng.component.ts
+++ portfolio/src/app/english/work-experience-eng/work-experience-eng.component.ts
@@ -7,14 +7,82 @@ import { Component } from '@angular/core';
 })
 export class WorkExperienceEngComponent {
 
+  workExperience = [
+    {
+      company:'Scientia Prop Traders',
+      sector:'',
+      backgroundImg: './assets/img/trading.jpg',
+      role:'Software Engineer',
+      startDate:'2019/12',
+      endDate:'2020/06',
+      tasks: 
+        [
+          'Automation of trading processes in MetaTrader4, mostly through the creation of visual indicators.',
+          'First approach to Neural Networks and Python.',
+          'Attempts to create a customized trading bot.',
+        ]
+    },
+    {
+      company:'Indra',
+      sector:'AI Department',
+      backgroundImg: './assets/img/indra_edificio.jpg',
+      role:'Data Scientist',
+      startDate:'2021/03',
+      endDate:'2021/06',
+      tasks: 
+        [
+          'Improvement of financial processes choosing the best Machine Learning algorithm and refactoring code for optimization. The programming language was Python.',
+          'Management of Local Virtual Environments using Bash.',
+          'Design of ETLs using AWS (Redshift) and SQL.'
+        ]
+    },
+    {
+      company:'Management Solutions',
+      sector:'Market Risk',
+      backgroundImg: './assets/img/ms_edificio.jpg',
+      role:'Data Engineer',
+      startDate:'2021/06',
+      endDate:'2022/03',
+      tasks: 
+        [
+          'Development of a tool to check FRTB requirements using Python.',
+          'Creation of GUIs in Python using Tkinter.',
+          'Design of ETLs in Google Cloud using Pandas, Scikit-Learn and Numpy for Data Management.'
+        ]
+    },
+    {
+      company:'KPMG',
+      sector:'Financial Services',
+      backgroundImg: './assets/img/kpmg_edificio.jpg',
+      role:'Data Engineer & FullStack Developer',
+      startDate:'2022/03',
+      endDate:'Now',
+      tasks: 
+        [
+          'Development of features for internal project.',
+          'Main tools: Angular (Front-End), Django (Back-End) and Microsoft SQL Server (Database).',
+          'Other tools: Git, AWS, Jira.',
+          'Two international projects 100% in English and Full Remote.',
+          'ETLs and analysis of data using SAS.',
+          'Client audited by ECB and PRA (IRB model validation for Credit Risk).'
+        ]
+    },
+  ]
+  
   timeInCompany(eDate: string, iDate: string){
-    let endDate = new Date(eDate);
-    let initDate = new Date(iDate);
+    let fixedIDate = (iDate+'/01').replaceAll('/','-')
+    let fixedEDate = ''
+    if (eDate !== 'Now'){
+      fixedEDate = (eDate+'/01').replaceAll('/','-');
+    }
+
+    let endDate = new Date(fixedEDate);
+    let initDate = new Date(fixedIDate);
     let months:number;
     let years:number;
     let time:string;
 
-    if (eDate === ""){
+    if (fixedEDate === ""){
       endDate = new Date();
       months = (endDate.getFullYear() - initDate.getFullYear())*12; // Result is in ms
       months += endDate.getMonth();
@@ -53,5 +121,4 @@ export class WorkExperienceEngComponent {
     }
     return time
   }
-
 }
